cd53080b156bbae6ff6a0582107644e9e0df0baa
NT_AUTHORITY <lukez@lukezhang.win>
2025-04-18 22:34:51 +0800

    v1.1.1
    Add commandblock listener support!

--- src/main/java/org/lukez/logX/CommandListener.java
+++ src/main/java/org/lukez/logX/CommandListener.java
@@ -2,11 +2,13 @@ package org.lukez.logX;
 
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.block.CommandBlock;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.server.ServerCommandEvent;
+import org.bukkit.command.BlockCommandSender;
 
 public class CommandListener implements Listener {
     private final LogX plugin;
@@ -30,5 +32,23 @@ public class CommandListener implements Listener {
                 command
         );
         LogX.getPluginLogger().info(logMessage);
+    }
+
+    @EventHandler
+    public void onCommandBlockCommand(ServerCommandEvent event) {
+        if (event.getSender() instanceof BlockCommandSender) {
+            BlockCommandSender cmdBlock = (BlockCommandSender) event.getSender();
+            Location loc = cmdBlock.getBlock().getLocation();
+            World world = loc.getWorld();
+            String worldName = LogX.getWorldAbbreviation(world.getName());
+            String command = event.getCommand();
+
+            String logMessage = String.format("[CB] [%s] %d,%d,%d: %s",
+                    worldName,
+                    loc.getBlockX(), loc.getBlockY(), loc.getBlockZ(),
+                    command
+            );
+            LogX.getPluginLogger().info(logMessage);
+        }
     }
 }
